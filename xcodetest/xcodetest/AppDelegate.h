0d1cd8840cb748ec12d700045ccb2cfd0aa1b306
Mac MIS 01 <wachisaw@gmail.com>
2014-04-08 09:54:23 +0700

    gergerg

---
+++ xcodetest/xcodetest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xcodetest
+//
+//  Created by MSC on 4/8/14.
+//  Copyright (c) 2014 MSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xcodetest/xcodetest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  xcodetest
+//
+//  Created by MSC on 4/8/14.
+//  Copyright (c) 2014 MSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
